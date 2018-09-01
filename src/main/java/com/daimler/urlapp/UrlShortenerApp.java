d349a7db16127f8c3dbb291f6d0a2c9baa5a5d76
Debasis Kar <d.kar@reply.de>
2018-09-01 13:22:27 +0200

    App refactored for CORS: allowed all origins

--- src/main/java/com/daimler/urlapp/UrlShortenerApp.java
+++ src/main/java/com/daimler/urlapp/UrlShortenerApp.java
@@ -2,7 +2,11 @@ package com.daimler.urlapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 
@@ -14,5 +18,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class UrlShortenerApp {
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApp.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
